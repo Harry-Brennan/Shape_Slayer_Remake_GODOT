91253e073a2277ba48dba02fe7583352c6457fbc
Harry Brennan <harrymb04@hotmail.co.uk>
2025-08-11 00:06:37 +0100

    Environmental wall collision actually working now yay.

--- Scripts/Environment_Wall.gd
+++ Scripts/Environment_Wall.gd
@@ -1,26 +1,17 @@
 @tool
-extends Area2D
+extends StaticBody2D
 
 @export var collision_vectors : Array[Vector2] : get = get_wall_collision_vectors , set = set_wall_collision_vectors
 
-@onready var wall_polygon : Polygon2D = %"Wall Polygon" 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	
-	for i in collision_vectors:
-		wall_polygon.polygon = collision_vectors
-	
-	print(wall_polygon.polygons)
-
-func _init():
-	for i in collision_vectors:
-		wall_polygon.polygon = collision_vectors
+@export var wall_polygon : Polygon2D
 
 func get_wall_collision_vectors() -> Array[Vector2]:
 	return collision_vectors
 
 func set_wall_collision_vectors(new_wall_collisions : Array[Vector2]):
 	collision_vectors = new_wall_collisions
+	if wall_polygon == null:
+		return
+	wall_polygon.polygon = collision_vectors
 	print("set_wall_collision_vectors called")
 	
